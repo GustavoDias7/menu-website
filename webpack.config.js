414b224fd891846b95b1460b6de9dc962faf6732
Gustavo Dias <gustavo7dias@gmail.com>
2024-11-25 10:54:53 -0300

    Move getPaths to the webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,37 @@ const path = require("path");
 const globAll = require("glob-all");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
-const { getPaths } = require("./utils/recursive-path.js");
 const { DefinePlugin } = require("webpack");
 const NunjucksWebpackPlugin = require("nunjucks-webpack-plugin");
 const { context } = require("./utils/constantes.js");
 const WatchExternalFilesPlugin = require("webpack-watch-files-plugin").default;
+const fs = require("fs");
+
+function getPaths(filePath) {
+  const entryPath = path.resolve(filePath);
+  const entryPaths = [];
+
+  // read the "entry file" content
+  const fileContent = fs.readFileSync(entryPath, "utf8") + "";
+
+  const paths = [];
+
+  // get all include and extends paths
+  const pattern = /\{%\s*(include|extends)\s+["']([^"']+?)["']\s*(?:\s+with\s+.+?)?\s*%\}/g;
+  const matches = fileContent.matchAll(pattern);
+  for (const match of matches) {
+    const absolutePath = path.resolve(path.parse(entryPath).dir, match[2]);
+    paths.push(absolutePath);
+  }
+
+  paths.forEach((item) => {
+    entryPaths.push(...getPaths(item));
+  });
+
+  entryPaths.push(entryPath);
+
+  return entryPaths;
+}
 
 module.exports = {
   context: __dirname,
